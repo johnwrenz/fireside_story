3d23ea9655dd170e0dc2d68f504281e559e52f54
John Renz <johnwrenz@gmail.com>
2017-09-19 15:03:24 -0500

    2nd commit

--- web-caesar/main.py
+++ web-caesar/main.py
@@ -2,6 +2,35 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+form ="""
+    <!DOCTYPE html>
+
+        <html>
+            <head>
+                <style>
+                    form {
+                        background-color: #eee;
+                        padding: 20px;
+                        margin: 0 auto;
+                        width: 540px;
+                        font: 16px sans-serif;
+                        border-radius: 10px;
+                    }
+                    textarea {
+                        margin: 10px 0;
+                        width: 540px;
+                        height: 120px;
+                    }
+                </style>
+            </head>
+            <body>
+                <form action="/add" method="POST">
+                    <input type="text" id="rot" name="rot"/>
+                    <input type="textarea" id="text" name="text"/>
+                create your form here -->
+            </body>
+        </html>   
+        """ 
 
 @app.route("/")
 def index():
